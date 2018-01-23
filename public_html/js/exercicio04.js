2b1756df782a8ac286fe8bee69fb5ce7b70a827c
a9130439 <a9130439@02LAB108PC14.rn.senac.br>
2018-01-22 21:21:02 -0300

    inicio

--- public_html/js/exercicio04.js
+++ public_html/js/exercicio04.js
@@ -1,5 +1,5 @@
 function exercicio04() {
-    
+
 //    Supondo que a população de um país A seja da ordem de 80000 
 //    habitantes com uma taxa anual de crescimento de 3% e que a 
 //    população de B seja 200000 habitantes com uma taxa de 
@@ -11,25 +11,25 @@ function exercicio04() {
 //Altere o programa anterior permitindo ao usuário informar as 
 //populações e as taxas de crescimento iniciais. Valide a entrada 
 //e permita repetir a operação.
-    
+
     var populacaoA = parseInt(prompt("Informe a população de A"));
     var txA = parseFloat(prompt("Informe a taxa de A"));
     var populacaoB = parseInt(prompt("Informe a população de B"));
-    var txB = parseFloat(prompt("Informe a taxa de B"));    
-    
+    var txB = parseFloat(prompt("Informe a taxa de B"));
+
     txA *= 100;
     txB *= 100;
-    
+
     var i = 0;
     while (populacaoA < populacaoB) {
         populacaoA += populacaoA * txA;
         populacaoB += populacaoB * txB;
-        i++; 
+        i++;
     }
     alert("Passaram " + i + " Anos");
 
-    
-    
+
+
 //    for(var i = 0; i < 10000; i++) {
 //        populacaoA += populacaoA * txA;
 //        populacaoB += populacaoB * txB;
@@ -49,4 +49,91 @@ function exemploWhile() {
     alert("Parabens! Você é uma pessoa inteligente!")
 }
 
+
+
+function exemplowhile() {
+//Faça um programa que peça uma nota, entre zero e dez. 
+//Mostre uma mensagem caso o valor seja inválido e 
+//continue pedindo até que o usuário informe um valor válido.
+    var nota;
+    do {
+        nota = parseFloat(prompt("digite a nota"));
+    } while (nota < 0 || nota > 10 || isNaN(nota))
+    {
+    }
+    alert(nota);
+}
+
+
+
+
+function exemplowhile02() {
+//Faça um programa que leia um nome de usuário e a 
+//sua senha e não aceite a senha igual ao nome do usuário, 
+//mostrando uma mensagem de erro e voltando a pedir as informações.   
+    var nome, senha;
+
+    do {
+        nome = prompt("Digite seu nome");
+        senha = prompt("Digite sua senha");
+        if (nome == senha) {
+            alert("nome e senha tem que ser diferentes");
+        }
+    } while (nome == senha);
+    document.write()("nome e senha diferentes");
+}
+
+
+
+function exemplowhile03() {
+//Faça um programa que leia e valide as seguintes informações:
+//Nome: maior que 3 caracteres;
+//Idade: entre 0 e 150;
+//Salário: maior que zero;
+//Sexo: 'f' ou 'm';
+//Estado Civil: 's', 'c', 'v', 'd';
+    nome = prompt("Digite seu nome");
+    while (nome.length <= 3 || !isNaN(nome)){
+        nome = prompt("Digite seu nome de novo");
+    }
+    idade = parseInt(prompt("Digite sua idade"));
+    while (idade < 0 || idade > 150) {
+        idade = prompt("Idade invalida, tem que ser uma idade maior que zero e menor que 150");
+    }
+    salario = parseFloat(prompt("Digite seu salario"));
+    while (salario < 0) {
+        salario = prompt("Salario nao pode ser menor que zero");
+    }
+    
+    var sexo;
+    do  {
+         sexo = prompt("Digite o sexo - m ou f");
+    }
+    while (sexo != "m" && sexo != "f") {    
+    }
+    if(sexo == "m") {
+        sexo = "Masculino";
+    } else {
+        sexo = "Feminino";
+    }
+    
+    estadoCivil = prompt("Digite o estado civil - s/c/v/d");
+    while (estadoCivil != "s" && estadoCivil != "c" && estadoCivil != "v" && estadoCivil != "d") {
+        estadoCivil = prompt("Estado civil invalido");
+    }
+    if(estadoCivil == "c") {
+        estadoCivil = "Casado";
+    } else if(estadoCivil == "s") {
+        estadoCivil = "Solteiro";
+    } else if(estadoCivil == "v") {
+        estadoCivil = "Viuvo";
+    } else if(estadoCivil == "d") {
+        estadoCivil = "Divorciado";
+    }
+    
+    document.write("Nome " +nome + "<br> Idade " + idade + " anos <br> Salario " + salario.toFixed(2) + "<br> Sexo " + sexo + "<br> Estado civil " + estadoCivil);
+  
+    
+    
+}
 
--- public_html/js/exerciciosFor.js
+++ public_html/js/exerciciosFor.js
@@ -1,84 +1,85 @@
 
+
+
+function exercicioFor01() {
 //Faça um programa que peça uma nota, entre zero e dez. 
 //Mostre uma mensagem caso o valor seja inválido e continue 
 //pedindo até que o usuário informe um valor válido.
 
-//function exercicio01() {
-//    
-//    for(var i = 0; i < 10; i++) {
-//        n1 = parseInt(prompt("Digite um número de 0 a 10"));
-//        if(n1 == 7) {
-//            alert("Você acertou");
-//            break;
-//        } else if(n1 > 10) {
-//            alert("Número invalido");
-//        } else {
-//            alert("Você errou tente novamente");
-//        }
-//    }  
-//}
+    for (var i = 0; i < 10; i++) {
+        n1 = parseInt(prompt("Digite um número de 0 a 10"));
+        if (n1 == 7) {
+            alert("Você acertou");
+            break;
+        } else if (n1 > 10) {
+            alert("Número invalido");
+        } else {
+            alert("Você errou tente novamente");
+        }
+    }
+}
 
+
+
+function exercicioFor02() {
 //Faça um programa que leia um nome de usuário e a sua senha e 
 //não aceite a senha igual ao nome do usuário, mostrando uma 
-//mensagem de erro e voltando a pedir as informações.
+//mensagem de erro e voltando a pedir as informações.    
 
-//function exercicio02() {
-//    
-//    for(i = 1; i < 5; i++){
-//        nome = prompt("Digite seu nome");
-//        senha = prompt("Digite sua senha");
-//        if(nome != senha) {
-//            alert("Acesso autorizado");
-//            break;
-//        } else {
-//            alert("Acesso negado");
-//        }
-//    } 
-//    
-//}
+    for (i = 1; i < 5; i++) {
+        nome = prompt("Digite seu nome");
+        senha = prompt("Digite sua senha");
+        if (nome != senha) {
+            alert("Acesso autorizado");
+            break;
+        } else {
+            alert("Acesso negado");
+        }
+    }
+}
 
+
+
+function exercicioFor03() {
 //Faça um programa que leia e valide as seguintes informações:
 //Nome: maior que 3 caracteres;
 //Idade: entre 0 e 150;
 //Salário: maior que zero;
 //Sexo: 'f' ou 'm';
-//Estado Civil: 's', 'c', 'v', 'd';
+//Estado Civil: 's', 'c', 'v', 'd';    
 
-function exercicio03() {
-    
-    for(i = 1; i < 2; i++) {
-        
-        for(a = 1; a < 50; a++) {
-        nome = prompt("digite seu nome");       
-            if(nome.length > 3) {              
+    for (i = 1; i < 2; i++) {
+
+        for (a = 1; a < 50; a++) {
+            nome = prompt("digite seu nome");
+            if (nome.length > 3) {
                 break;
-            }           
-            else {
+            } else {
                 alert("Nome invalido");
-            }      
+            }
         }
-        
-        for(a = 1; a < 50; a++) {
-        idade = parseInt(prompt("Digite sua idade"));
-            if(idade > 0 && idade < 150) {
+
+        for (a = 1; a < 50; a++) {
+            idade = parseInt(prompt("Digite sua idade"));
+            if (idade > 0 && idade < 150) {
                 break;
             } else {
                 alert("Idade invalida tente denovo");
             }
         }
-        
-        for(a = 1; a < 50; a++) {
-        salario = parseFloat(prompt("Digite seu salario"));
-            if(salario > 0) {
-                break; 
+
+        for (a = 1; a < 50; a++) {
+            salario = parseFloat(prompt("Digite seu salario"));
+            if (salario > 0) {
+                break;
             } else {
                 alert("Salario não pode ser zero ou menos");
             }
         }
-        
-        for(a = 1; a < 50; a++) {
-        sexo = prompt("Digite o sexo m/f");
-            if(sexo.toUpperCase() === "M") {
+
+        for (a = 1; a < 50; a++) {
+            sexo = prompt("Digite o sexo m/f");
+            if (sexo.toUpperCase() === "M") {
                 sexo = "Masculino";
                 break;
             } else if (sexo.toUpperCase() === "F") {
@@ -87,35 +88,57 @@ function exercicio03() {
             } else {
                 alert("Sexo invalido digite m/f");
             }
-        
+
         }
-        
-        for(a = 1; a < 50; a++) {
-        estado = prompt("Estado civil - s / c / v / d");
-            if(estado.toUpperCase() === "S") {
+
+        for (a = 1; a < 50; a++) {
+            estado = prompt("Estado civil - s / c / v / d");
+            if (estado.toUpperCase() === "S") {
                 estado = "Solteiro(a)";
-            break;
-        } else if(estado.toUpperCase() === "C") {
+                break;
+            } else if (estado.toUpperCase() === "C") {
                 estado = "Casado(a)";
-            break;
-        } else if(estado.toUpperCase() === "V") {
+                break;
+            } else if (estado.toUpperCase() === "V") {
                 estado = "Viuvo(a)";
-            break;
-        } else if(estado.toUpperCase() === "D") {
+                break;
+            } else if (estado.toUpperCase() === "D") {
                 estado = "Divorciado(a)";
-            break;
-        } else {
-            alert("Opção invalida tente denovo ");
-        }      
-    }
-    
+                break;
+            } else {
+                alert("Opção invalida tente denovo ");
+            }
+        }
+
         alert("Bem vindo cadastro feito com sucesso");
-        alert("Nome " + nome + "\n Idade " + idade + " anos \n Salario " + salario.toFixed(2) + "\n Sexo " + sexo + "\n Estsdo cívil " + estado);   
+        alert("Nome " + nome + "\n Idade " + idade + " anos \n Salario " + salario.toFixed(2) + "\n Sexo " + sexo + "\n Estsdo cívil " + estado);
+    }
+}
+
+function exemploFor06() {
+//Faça um programa que imprima na tela os números de 1 a 20, 
+//um abaixo do outro. Depois modifique o programa para que 
+//ele mostre os números um ao lado do outro.
+    for (var i = 1; i <= 20; i++) {
+        document.write(" - " + i);
     }
 }
 
 
+function exemploFor07() {
+//Faça um programa que leia 5 números e informe o maior número.
+    var maior = 0;
+    for (i = 0; i < 5; i++) {
+        aux = parseInt(prompt("informe um número " + i));      
+        if(aux > maior){
+        maior = aux;   
+        }
+         document.write(maior); 
+    }
 
+      
+        
+    }
 
 
 
